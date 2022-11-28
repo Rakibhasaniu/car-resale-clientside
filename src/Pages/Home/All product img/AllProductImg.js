19b745bf2dc0e98f83625027536c10dba376581c
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 18:53:57 +0600

    all cars added

--- src/Pages/Home/All product img/AllProductImg.js
+++ src/Pages/Home/All product img/AllProductImg.js
@@ -12,13 +12,13 @@ const AllProductImg = () => {
                 setImg(data)
             })
     }, [])
-    console.log(imgss)
+    
 
     return (
         <>
             <section className=" dark:bg-gray-800 dark:text-gray-50 py-10">
-                <h1 className='text-4xl text-center pb-2'>All ReseLL Collection - 2022</h1>
-                <p className='text-[22px] text-amber-600 text-center pb-5'>Click This img or zooming product </p>
+                <h1 className='text-4xl text-center pb-2'>Car Collection</h1>
+                <p className='text-[22px] text-amber-600 text-center pb-5'>Click Those Image To See Full Image </p>
                 <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4 md:px-20 px-2">
                     {
                         imgss?.map((imgs, i) => <>
