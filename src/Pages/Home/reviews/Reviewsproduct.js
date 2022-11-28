e349ea57e19aaa9da2872cc8e96a574fce5bb919
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 19:50:36 +0600

    review added

--- src/Pages/Home/reviews/Reviewsproduct.js
+++ src/Pages/Home/reviews/Reviewsproduct.js
@@ -15,7 +15,7 @@ const Revewsproduct = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
                                         <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                                         <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                                    </svg>The wedding is a ceremony where two people are united in marriage. Wedding traditions and customs vary greatly between cultures, ethnic groups, religions, countries,
+                                    </svg>The BMW 3 Series is one of the best posh saloon cars on sale, it’s packed with tech, is great to drive and spacious enough for a family of four. Some options are rather pricey but every version, excepting the base model, comes standard with an 8-speed automatic gearbox.
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
                                         <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                                         <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
