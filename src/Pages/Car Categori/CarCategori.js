13cbdf263ac3fbcd7093b0197aa51d3555cef55c
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 21:35:16 +0600

    btn chnages

--- src/Pages/Car Categori/CarCategori.js
+++ src/Pages/Car Categori/CarCategori.js
@@ -20,10 +20,6 @@ const CarCategori = () => {
   return (
     <>
       <div className="container my-[100px] ">
-        {/* <h1 className="text-4xl text-gray-700 text-bold font-mono py-5 text-center">
-          All Categories <span className="text-amber-600">Resell Car</span>{" "}
-          Collection{" "}
-        </h1> */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 md:px-12 px-5 justify-center items-center">
           {services?.map((item, i) => (
             <>
@@ -70,7 +66,7 @@ const CarCategori = () => {
                     <div className="flex justify-center mt-3">
                       <Link to={`/cetegories/${item?.id}`} key={item?.id}>
                         <button className="btn md:px-12 btn-primary my-2">
-                          viewss all cars
+                          See All Cars
                         </button>
                       </Link>
                     </div>
@@ -80,58 +76,6 @@ const CarCategori = () => {
             </>
           ))}
 
-          {/* <div className="max-w-lg p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 rounded-lg">
-                        <div className="flex justify-between pb-4 border-bottom">
-                            <div className="flex items-center">
-                                <a rel="noopener noreferrer" href="#" className="mb-0 capitalize dark:text-gray-100">Replles<span className='text-amber-600'> Tesla Car</span></a>
-                            </div>
-                            <a rel="noopener noreferrer" href="#">Replles Car 3</a>
-                        </div>
-                        <div className="space-y-4">
-                            <div className="space-y-2">
-                                <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
-                                <div className="flex items-center text-xs justify-between">
-                                    <span>Uplode Date : 10/02/2022</span>
-                                    <span>5 day ago</span>
-                                </div>
-                            </div>
-                            <div className="space-y-2">
-                                <a rel="noopener noreferrer" href="#" className="block">
-                                    <h3 className="text-xl font-semibold dark:text-violet-400">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
-                                </a>
-                                <p className="leading-snug dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
-                                <div className="flex justify-center mt-3" >
-                                    <button className="btn md:px-12 btn-primary">viewss all cars</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="max-w-lg p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 rounded-lg">
-                        <div className="flex justify-between pb-4 border-bottom">
-                            <div className="flex items-center">
-                                <a rel="noopener noreferrer" href="#" className="mb-0 capitalize dark:text-gray-100">Replles<span className='text-amber-600'> Tesla Car</span></a>
-                            </div>
-                            <a rel="noopener noreferrer" href="#">Replles Car 3</a>
-                        </div>
-                        <div className="space-y-4">
-                            <div className="space-y-2">
-                                <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
-                                <div className="flex items-center text-xs justify-between">
-                                    <span>Uplode Date : 10/02/2022</span>
-                                    <span>5 day ago</span>
-                                </div>
-                            </div>
-                            <div className="space-y-2">
-                                <a rel="noopener noreferrer" href="#" className="block">
-                                    <h3 className="text-xl font-semibold dark:text-violet-400">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
-                                </a>
-                                <p className="leading-snug dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
-                                <div className="flex justify-center mt-3" >
-                                    <button className="btn md:px-12 btn-primary">viewss all cars</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
         </div>
       </div>
     </>
