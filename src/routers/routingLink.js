e19d21b9713cf58aa57379c52423ca1c28fdb0a6
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 19:58:50 +0600

    categories added

--- src/routers/routingLink.js
+++ src/routers/routingLink.js
@@ -27,7 +27,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                // element: <Home></Home>
+                element: <CarCategori></CarCategori>
             },
             {
                 path: '/cetegories',
